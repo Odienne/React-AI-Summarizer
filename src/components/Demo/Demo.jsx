1ebf65122e8575d2559119cf30fb3de816b6da11
Adam Odienne <odienne.adam@gmail.com>
2026-03-02 18:39:26 +0100

    add main UI and provider with API to query the summarize RAPID API endpoint

--- src/components/Demo/Demo.jsx
+++ src/components/Demo/Demo.jsx
@@ -2,23 +2,51 @@ import React from 'react';
 
 import { useState, useEffect } from 'react';
 import { copy, linkIcon, loader, tick } from '../../assets';
+import { useLazyGetSummaryQuery } from '../../services/article';
 
 function Demo() {
   const [article, setArticle] = useState({
     url: '',
     summary: '',
   });
+  const [lang, setLang] = useState('fr');
+  const [length, setLength] = useState(0);
+
+  const [allArticles, setAllArticles] = useState([]);
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   const handleSubmit = async (e) => {
-    console.log('submit');
-    
+    e.preventDefault();
+    if (!article.url) return;
+
+    const data = await getSummary({ articleUrl: article.url, lang: lang, length: length });
+
+    if (data?.data?.summary) {
+      const newArticle = { ...article, summary: data.data.summary };
+      setArticle(newArticle);
+
+      // also add new article to history
+      const updatedAllArticles = [newArticle, ...allArticles];
+      setAllArticles(updatedAllArticles);
+      //and save to localStorage
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
+    }
   };
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
 
   return (
     <section className='mt-16 w-full max-w-xl'>
       {/* search */}
       <div className='flex flex-col w-full gap-2'>
-        <form className='relative flex justify-center items-center' onSubmit={() => {}}>
+        <form className='relative flex justify-center items-center' onSubmit={handleSubmit}>
           <img src={linkIcon} alt='link_icon' className='absolute left-0 my-2 ml-3 w-5' />
 
           <input
@@ -26,14 +54,13 @@ function Demo() {
             placeholder='Enter a url'
             value={article.url}
             onChange={(e) => {
-              setArticle({...article, url: e.target.value})
+              setArticle({ ...article, url: e.target.value });
             }}
             className='url_input peer'
           />
 
           <button
-            type='button'
-            onSubmit={handleSubmit}
+            type='submit'
             className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'
           >
             <p>↵</p>
@@ -41,8 +68,40 @@ function Demo() {
         </form>
 
         {/* browse url history */}
+        <div className='flex flex-col gap-1 max-h-50 overflow-y-auto'></div>
+        {allArticles.map((article, index) => (
+          <div className='link_card' key={`link-${index}`} onClick={() => setArticle(article)}>
+            <div className='copy_btn'>
+              <img src={copy} alt='copy_icon' className='w-[40%] h-[40%] object-contain' />
+            </div>
+            <p className='flex-1 font-medium font-satoshi text-bmie-700 text-sm truncate'>
+              {article.url}
+            </p>
+          </div>
+        ))}
       </div>
       {/* results */}
+      <div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? (
+          <p className='font-inter font-bold tetx-black text-center'>
+            An error occured... <br />
+            <span className='font-satoshi font-normal text-gray-700'>{error.data?.error}</span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 }
---
+++ src/services/article.jsx
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const BASE_URL = 'https://article-extractor-and-summarizer.p.rapidapi.com/';
+const HOST = 'article-extractor-and-summarizer.p.rapidapi.com';
+const KEY = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+
+
+export const articleApi = createApi({
+  reducerPath: 'articleApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', KEY);
+      headers.set('X-RapidAPI-Host', HOST);
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=${params.length}&lang=${params.lang}`,
+    }),
+  }),
+});
+
+
+//lazy to fire it on demand
+export const {useLazyGetSummaryQuery} = articleApi;
